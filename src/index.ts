977ad8d47392c6e5432dd6a5b14f4adf1ac2550f
Kees Kluskens <kees@kees.im>
2025-09-18 08:47:11 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,13 @@ import { MouseHookEventMap } from "./types.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const require = createRequire(import.meta.url);
-const addon = require(join(__dirname, "../build/Release/mouse_hook.node"));
 
 class MouseHook extends EventEmitter<MouseHookEventMap> {
   #hook;
-  constructor() {
+  constructor(nodeFile?: string) {
     super();
+    const addon = require(nodeFile ??
+      join(__dirname, nodeFile ?? "../build/Release/mouse_hook.node"));
     this.#hook = new addon.Hook((evt: any) => this.emit(evt.type, evt));
   }
   start() {
